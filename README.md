e6ce1d93808cde814e41204366f90edcf33e74f0
Reisu1337 <73112598+reisu1337@users.noreply.github.com>
2021-07-30 20:43:53 +0000

    Final working version

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# NOT YET WORKING
-
-
 # TwitchAPICall - https://replit.com/@reisu1337/TwitchAPICall
 A simple call to the Twitch API to check if a user is live. Great for use in conjunction with the discord.py library for a Discord bot, or any other chatbot/automotation function.
 Free to use, edit and redistribute in line with the MIT License.  
@@ -9,7 +6,9 @@ Free to use, edit and redistribute in line with the MIT License.
 
 2) Copy your client ID and generate a client secret, and put them into the CLIENT_ID and CLIENT_SECRET variables, if publishing publicly consider using enivronment variables for security - https://docs.replit.com/archive/secret-keys  
 
-3) Replace the name "reisu1337" with the name of the Twitch user you would like to track  
+3) At the bottom of the code, call the isLive function with the username of the streamer passed as a string, I.E  
+
+`isLive("reisu1337")`
 
 ## Socials
 Twitch - https://www.twitch.tv/reisu1337  
--- twitchCall.py
+++ twitchCall.py
@@ -1,29 +1,30 @@
 import os
 import requests
 
-URL = "https://id.twitch.tv/oauth2/token"
-CLIENT_ID = os.environ['CLIENT_ID']
-CLIENT_SECRET = os.environ['CLIENT_SECRET']
-GRANT_TYPE = "client_credentials"
-PARAMS = {
-  "client_id": CLIENT_ID,
-  "client_secret": CLIENT_SECRET,
-  "grant_type": GRANT_TYPE
-}
+def isLive(username):
+  URL = "https://id.twitch.tv/oauth2/token"
+  CLIENT_ID = os.environ['CLIENT_ID']
+  CLIENT_SECRET = os.environ['CLIENT_SECRET']
+  GRANT_TYPE = "client_credentials"
+  PARAMS = {
+      "client_id": CLIENT_ID,
+      "client_secret": CLIENT_SECRET,
+      "grant_type": GRANT_TYPE
+  }
 
-r1 = requests.post(url = URL, params = PARAMS)
+  r1 = requests.post(url=URL, params=PARAMS)
 
-token = r1.json()["access_token"]
-print(token)
+  token = r1.json()["access_token"]
 
-URL2 = "https://api.twitch.tv/helix/search/channels?query=reisu1337"
-PARAMS2 = {
-  "client-id": CLIENT_ID,
-  "Authorization": f"Bearer {token}"
-}
+  URL2 = "https://api.twitch.tv/helix/search/channels?query="+username
+  HEADERS = {"client-id": CLIENT_ID, "Authorization": f"Bearer {token}"}
 
-r2 = requests.post(url = URL2, params = PARAMS2)
+  r2 = requests.get(url=URL2, headers=HEADERS)
 
-data = r2.json()
+  data = r2.json()
 
-print(data)
+  for i in range(len(data["data"])):
+    if (data["data"][i]["broadcaster_login"]) == username or (data["data"][i]["display_name"]) == username:
+      return (data["data"][i]["is_live"])
+
+print(isLive("reisu1337"))
