fc4d587197d8ae7289ce3ebab199245f9db954b3
Reisu1337 <73112598+reisu1337@users.noreply.github.com>
2021-07-30 17:02:00 +0000

    edited main

--- twitchCall.py
+++ twitchCall.py
@@ -16,15 +16,13 @@ r1 = requests.post(url = URL, params = PARAMS)
 token = r1.json()["access_token"]
 print(token)
 
-string1 = "Bearer "+str(token)
-
 URL2 = "https://api.twitch.tv/helix/search/channels?query=reisu1337"
 PARAMS2 = {
-  "client_id": CLIENT_ID,
-  "Authorization": string1
+  "client-id": CLIENT_ID,
+  "Authorization": f"Bearer {token}"
 }
 
-r2 = requests.get(url = URL2, params = PARAMS2)
+r2 = requests.post(url = URL2, params = PARAMS2)
 
 data = r2.json()
 
